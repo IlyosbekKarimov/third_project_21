09e483611a87286e6aba5c3f2f2142c12e28247e
Ilyosbek <skeevesp@student.21-school.ru>
2024-02-18 19:13:05 +0500

    crack.c file created

---
+++ src/crack.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float x, y;
+    if (scanf("%f %f", &x, &y) != 2) {
+        printf("n/a");
+        return 0;
+    }
+    if (x * x + y * y <= 25) {
+        printf("GOTCHA");
+    } else {
+        printf("MISS");
+    }
+    return 0;
+}
