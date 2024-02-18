546cca122a4f9264de8ef71b4ec27eb3a0130c68
Ilyosbek <skeevesp@student.21-school.ru>
2024-02-18 18:53:16 +0500

    Modified

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
 int sum(int a, int b);
+int difference(int a, int b);
+int product(int a, int b);
 
-int main()
-{
+int main() {
+    int a, b;
+    if (scanf("%d %d", &a, &b) != 2) {
+        printf("n/a");
+        return 0;
+    }
+    if (b != 0) {
+        printf("%d %d %d %d", sum(a, b), difference(a, b), product(a, b), a / b);
+    } else {
+        printf("%d %d %d n/a", sum(a, b), difference(a, b), product(a, b));
+    }
+
     return 0;
 }
 
-int sum(int a, int b){
-    return (2 * a * b) / (a - b);
-}
+int sum(int a, int b) { return a + b; }
+int difference(int a, int b) { return a - b; }
+int product(int a, int b) { return a * b; }
